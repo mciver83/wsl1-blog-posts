4bd0912c8fa56baee11c4f4c9d8e0a23e99f3a88
Mark McIver <mark@Marks-MacBook-Pro.local>
2018-08-09 11:06:18 -0600

    authentication working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  login = () => {
+    let auth0domain = `https://${process.env.REACT_APP_AUTH0_DOMAIN}`
+    let clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+    let scope = encodeURIComponent('openid profile email')
+    let redirectUri = encodeURIComponent(`${window.location.origin}/auth/callback`)
+
+    let location = `${auth0domain}/authorize?client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}&response_type=code`
+
+    window.location = location
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +21,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.login}>login</button>
       </div>
     );
   }
