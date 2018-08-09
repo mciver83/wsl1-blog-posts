fb221468f57f11c971819886a6454a91e321cdb0
Mark McIver <mark@Marks-MacBook-Pro.local>
2018-08-09 11:55:44 -0600

    able to get and create posts

--- server/controllers/PostsCtrl.js
+++ server/controllers/PostsCtrl.js
@@ -1,8 +1,26 @@
 module.exports = {
-  read: (req, res) => {
-
+  read: async (req, res) => {
+    try {
+      let db = req.app.get('db')
+      let posts = await db.getPosts()
+      res.send(posts)
+    } catch (error) {
+      console.log('error fetching posts:', error)
+      res.status(500).send(error)
+    }
   },
-  create: (req, res) => {
-    
+  create: async (req, res) => {
+    try {
+      let db = req.app.get('db')
+      let { title, content } = req.body
+      let user_id = req.session.user ? req.session.user.id : 1
+
+      let newPost = { user_id, title, content }
+      let posts = await db.createPost(newPost)
+      res.send(posts[0])
+    } catch (error) {
+      console.log('error creating post:', error)
+      res.status(500).send(error)
+    }
   }
 }
